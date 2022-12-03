d03157a72d69601eb6f11f5766f549ca8645d10f
luucaslul <112034082+luucaslul@users.noreply.github.com>
2022-12-02 21:17:26 -0300

    arquivos extras

---
+++ anagrama/PlayerDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anagrama.DTO
+{
+    internal class PlayerDTO
+    {
+        public int ID
+        {
+            get; set;
+        }
+
+        public string Nome
+        {
+            get; set;
+        }
+
+        public int Tempo
+        {
+            get; set;
+        }
+    }
+}
