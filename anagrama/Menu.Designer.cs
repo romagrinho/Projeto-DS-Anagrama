bdeb1b916e72eed23e7b343cec7cb31a53cefc68
Lucas <lucasfanjos0022@gmail.com>
2022-12-02 21:14:52 -0300

    ranking pronto
    faltando teste

--- anagrama/Menu.Designer.cs
+++ anagrama/Menu.Designer.cs
@@ -181,6 +181,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
             this.ClientSize = new System.Drawing.Size(1005, 474);
             this.ControlBox = false;
